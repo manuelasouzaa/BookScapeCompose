2ef525dd0f337e803ec06e5fd0b83bcc999d96f5
Manuela Souza <manuela.souza@fulltime.com.br>
2025-02-27 10:26:47 -0300

    Criando o componente BookScapeTextField

---
+++ app/src/main/java/br/com/bookscapecompose/ui/components/TextField.kt
@@ -0,0 +1,57 @@
+package br.com.bookscapecompose.ui.components
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun BookScapeTextField(
+    modifier: Modifier = Modifier,
+    searchText: String,
+    onSearchChange: (String) -> Unit
+) {
+    TextField(
+        value = searchText,
+        onValueChange = {
+            onSearchChange(it)
+        },
+        label = {
+            Text(
+                text = "Search a book or an author",
+                fontSize = 16.sp
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+            )
+        },
+        modifier = modifier,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            unfocusedContainerColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            focusedLabelColor = MaterialTheme.colorScheme.onBackground,
+            unfocusedLabelColor = MaterialTheme.colorScheme.onBackground,
+            focusedTextColor = MaterialTheme.colorScheme.onTertiary,
+            unfocusedTextColor = MaterialTheme.colorScheme.onTertiary,
+            focusedLeadingIconColor = MaterialTheme.colorScheme.onPrimary,
+            unfocusedLeadingIconColor = MaterialTheme.colorScheme.onPrimary,
+            cursorColor = MaterialTheme.colorScheme.onPrimary,
+        ),
+        textStyle = TextStyle(
+            fontSize = 20.sp
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+    )
+}

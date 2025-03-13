e2426f3ed9f93632545aa8753b1c643801ff0c22
Manuela Souza <manuela.souza@fulltime.com.br>
2025-03-13 11:19:31 -0300

    Alterando a BookListScreen para que o CircularProgressIndicator fique no centro da tela

--- app/src/main/java/br/com/bookscapecompose/ui/screens/BookListScreen.kt
+++ app/src/main/java/br/com/bookscapecompose/ui/screens/BookListScreen.kt
@@ -1,14 +1,16 @@
 package br.com.bookscapecompose.ui.screens
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import br.com.bookscapecompose.ui.components.BookScapeList
@@ -30,7 +32,10 @@ fun BookListScreen(
     viewModel.showBooks(context)
 
     if (loading.value)
-        CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+        Column (Modifier.fillMaxSize(), Arrangement.Center, Alignment.CenterHorizontally){
+            CircularProgressIndicator(modifier = Modifier)
+        }
+
 
     if (!loading.value)
         BookScapeList(

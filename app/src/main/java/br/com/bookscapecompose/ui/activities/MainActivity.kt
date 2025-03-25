bc2b261c53cfb9e37f59ceb4669a7fa73d41a760
Manuela Souza <manuela.souza@fulltime.com.br>
2025-03-25 15:01:21 -0300

    Configurando a SplashScreen

--- app/src/main/java/br/com/bookscapecompose/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/bookscapecompose/ui/activities/MainActivity.kt
@@ -8,14 +8,30 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import br.com.bookscapecompose.ui.navigation.BookScapeNavHost
 import br.com.bookscapecompose.ui.theme.BookScapeComposeTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        val keepSplashScreen = mutableStateOf(true)
+
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition { keepSplashScreen.value }
+
+        lifecycleScope.launch {
+            delay(100)
+            keepSplashScreen.value = false
+        }
+
         enableEdgeToEdge()
         setContent {
             BookScapeComposeTheme {
